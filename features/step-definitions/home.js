345e1e4d620b8ef1ebf701a97aa40326f282c792
rohandambekar <77868656+rohandambekar@users.noreply.github.com>
2021-01-23 14:07:45 +1100

    Create home.js

---
+++ features/step-definitions/home.js
@@ -0,0 +1,101 @@
+const { Given, When, Then } = require('cucumber');
+const assert = require('assert')
+const homePage = require('../pageobjects/home.page');
+const settingsPage = require('../pageobjects/settings.page');
+
+
+
+Given ("I navigated to duckduckgo website", () => {
+
+    browser.url("https://start.duckduckgo.com/")
+    
+});
+
+When ("I am on the home page", () => {
+
+    const title= browser.getTitle()
+    assert.strictEqual('DuckDuckGo — Privacy, simplified.', title, 'Page title is not displayed correctly')
+});
+
+When ("I look at the page", () => {
+
+});
+
+
+Then ("I expect to see the duckduckgo logo on the page", () => {
+    
+    homePage.duckduckgoLogo.waitForDisplayed({timeout: 10000})
+    assert.strictEqual(true, homePage.duckduckgoLogo.isDisplayed(), 'duckduckgo logo is not displayed on Home page')
+});
+
+
+When ("I type super into the search box", () => {
+
+    homePage.searchBox.setValue('super')
+
+});
+
+Then ("I expect to see exactly 10 suggestions in the typeahead dropdown", () => {
+    
+    assert.strictEqual(true, homePage.suggesstionsList.isDisplayed(), 'Suggesstions list is not displayed after entring Super text')
+});
+
+
+When ("I type supercalafragalistic into the search box", () => {
+
+    homePage.searchBox.setValue('supercalafragalistic')
+
+});
+
+Then ("I expect the first result to be supercalafragalisticexpialadoshus", () => {
+    
+    assert.strictEqual('supercalafragalisticexpialadoshus', homePage.expectedFirstSearchResult.getText(), 'Expected Text is not displayed after entring supercalafragalistic text')
+});
+
+When ("I click on the hamburger menu in the top right", () => {
+
+    homePage.hamburgerMenu.click()
+
+});
+
+Then ("I expect to see a themes link", () => {
+    
+    homePage.themesLink.waitForDisplayed({timeout: 10000})
+    assert.strictEqual(true, homePage.themesLink.isDisplayed(), 'Themes link is not displayed after clicking on the hamburger menu')
+});
+
+
+When ("I click on the themes link then click on the dark mode theme button", () => {
+
+    homePage.hamburgerMenu.click()
+    homePage.themesLink.click()
+
+});
+
+
+Then ("I click on the dark mode theme button", () => {
+    
+    settingsPage.darkLabel.click()
+    settingsPage.saveAndExitBtn.click()
+});
+
+
+
+Then ("My page background should change colour", () => {
+    
+    const title= browser.getTitle()
+    assert.strictEqual('DuckDuckGo — Privacy, simplified.', title, 'Page title is not displayed correctly')
+    assert.strictEqual(true, settingsPage.darkBackground.isDisplayed(), 'Background colour is not changed')
+});
+
+
+Then ("I click on the dark mode theme button", () => {
+    
+    
+});
+
+
+Then ("I click on the dark mode theme button", () => {
+    
+    
+});
